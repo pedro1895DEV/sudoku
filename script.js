115f0de8b0a9309fdc7944b2ccc56ca862a03b0f
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2022-12-05 00:05:36 -0300

    sudoku

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ $(document).ready(
     function (){
         for(let i = 1; i <= 9; i++){
             let tr = $('<tr>');
-            for(let j =1; j <=9; j++){
-                let td = $(`<td> <input type = "text" id="c${i}${j}"`)
+            for(let j = 1; j <= 9; j++){
+                let td = $(`<td> <input type="text" id="c${i}${j}"</td>`)
                 tr.append(td);
             }
 
             $('#grade').append(tr);
         }
     }
-)
+);
